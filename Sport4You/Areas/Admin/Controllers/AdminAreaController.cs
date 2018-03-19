523e7c5fdbe6695e05686d723ed51810ae159a2f
mathsfreak7 <mathsfreak7@gmail.com>
2018-03-19 03:44:34 +0100

    mdf file

---
+++ Sport4You/Areas/Admin/Controllers/AdminAreaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sport4You.Areas.Admin.Controllers
+{
+    public class AdminAreaController : Controller
+    {
+        // GET: Admin/AdminArea
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
